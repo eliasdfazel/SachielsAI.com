5eb138ffe8d1ae5118670dd9d080b6fdd547087e
Elias Fazel <elias.fazel@gmail.com>
2023-10-07 11:25:27 -0700

    [Description]
    [ChangeLog]
    ~ Sachiels AI Website
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sachiel_website/SplashScreen.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
