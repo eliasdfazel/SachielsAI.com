83c2fcf74b267040dbd93922952afbe30b78441c
Elias Fazel <elias.fazel@gmail.com>
2023-10-15 04:54:35 -0700

    [Description]
    [ChangeLog]
    ~ Sachiels AI Website
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/dashboard/SplashScreen.dart
+++ lib/dashboard/SplashScreen.dart
@@ -1,4 +1,3 @@
-import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 import 'package:sachiel_website/dashboard/sections/purchase_plan_picker.dart';
 import 'package:sachiel_website/resources/colors_resources.dart';
@@ -13,24 +12,39 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
 
-  String generatedVerificationId = "";
+  late AnimationController animationController;
+
+  late Animation<Offset> offsetAnimation;
+  late Animation<double> doubleAnimation;
+
+  bool menuOpen = false;
 
   @override
   void initState() {
     super.initState();
 
+    animationController = AnimationController(vsync: this,
+        duration: const Duration(milliseconds: 777),
+        reverseDuration: const Duration(milliseconds: 333),
+        animationBehavior: AnimationBehavior.preserve);
+
+    offsetAnimation = Tween<Offset>(begin: const Offset(0, 0), end: const Offset(0.49, 0))
+        .animate(CurvedAnimation(
+          parent: animationController,
+          curve: Curves.easeIn
+        ));
+    doubleAnimation = Tween<double>(begin: 1, end: 0.91)
+        .animate(CurvedAnimation(
+            parent: animationController,
+            curve: Curves.easeOut
+        ));
+
   }
 
   @override
   Widget build(BuildContext context) {
-
-    Future.delayed(const Duration(seconds: 19), () {
-
-      // launchUrlString("https://play.google.com/store/apps/details?id=co.geeksempire.sachiel.signals", mode: LaunchMode.externalApplication);
-
-    });
 
     return SafeArea(
         child: MaterialApp(
@@ -49,185 +63,182 @@ class _SplashScreenState extends State<SplashScreen> {
                 resizeToAvoidBottomInset: false,
                 backgroundColor: ColorsResources.black,
                 body: Stack(
-                    children: [
+                  children: [
 
-                      /* Start - Gradient Background - Dark */
-                      Container(
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(17),
-                              topRight: Radius.circular(17),
-                              bottomLeft: Radius.circular(17),
-                              bottomRight: Radius.circular(17)
-                          ),
-                          border: Border(
-                              top: BorderSide(
-                                color: ColorsResources.black,
-                                width: 7,
-                              ),
-                              bottom: BorderSide(
-                                color: ColorsResources.black,
-                                width: 7,
-                              ),
-                              left: BorderSide(
-                                color: ColorsResources.black,
-                                width: 7,
-                              ),
-                              right: BorderSide(
-                                color: ColorsResources.black,
-                                width: 7,
-                              )
-                          ),
-                          gradient: LinearGradient(
-                              colors: [
-                                ColorsResources.premiumDark,
-                                ColorsResources.black,
-                              ],
-                              begin: FractionalOffset(0.0, 0.0),
-                              end: FractionalOffset(1.0, 0.0),
-                              stops: [0.0, 1.0],
-                              transform: GradientRotation(-45),
-                              tileMode: TileMode.clamp
-                          ),
-                        ),
-                      ),
-                      /* End - Gradient Background - Dark */
+                    /* Start - Menu Items */
+                    menuItems(),
+                    /* End - Menu Items */
 
-                      /* Start - Branding Transparent */
-                      Align(
-                        alignment: Alignment.center,
-                        child: Opacity(
-                            opacity: 0.1,
-                            child: Transform.scale(
-                                scale: 1.7,
-                                child: const Image(
-                                  image: AssetImage("assets/logo.png"),
-                                )
-                            )
-                        ),
-                      ),
-                      /* End - Branding Transparent */
+                    /* Start - Contents Widgets */
+                    allContentsWidgets(),
+                    /* End - Contents Widgets */
 
-                      /* Start - Gradient Background - Golden */
-                      Align(
-                          alignment: Alignment.topRight,
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: const BorderRadius.only(
-                                    topLeft: Radius.circular(17),
-                                    topRight: Radius.circular(17),
-                                    bottomLeft: Radius.circular(17),
-                                    bottomRight: Radius.circular(17)
-                                ),
-                                gradient: RadialGradient(
-                                  radius: 1.1,
-                                  colors: <Color> [
-                                    ColorsResources.primaryColorLighter.withOpacity(0.51),
-                                    Colors.transparent,
-                                  ],
-                                  center: const Alignment(0.79, -0.87),
-                                )
-                            ),
-                            child: SizedBox(
-                              height: calculatePercentage(99, displayHeight()),
-                              width: calculatePercentage(99, displayWidth()),
-                            ),
-                          )
-                      ),
-                      /* End - Gradient Background - Golden */
-
-                      /* Start - Purchase Plan Picker */
-                      const Positioned(
-                          right: 19,
-                          top: 19,
-                          child: PurchasePlanPicker()
-                      ),
-                      /* End - Purchase Plan Picker */
-
-                      /* Start - Menu */
-                      Positioned(
-                          left: 19,
-                          top: 19,
-                          child: SizedBox(
-                              height: 59,
-                              width: 59,
-                              child: InkWell(
-                                  onTap: () {
-
-
-
-                                  },
-                                  child: const Image(
-                                    image: AssetImage("assets/menu.png"),
-                                  )
-                              )
-                          ),
-                      ),
-                      /* End - Menu */
-
-                      /* Start - Coming Soon */
-                      Center(
-                          child: Padding(
-                              padding: const EdgeInsets.fromLTRB(37, 0, 37, 0),
-                              child: Blur(
-                                  blur: 5,
-                                  blurColor: ColorsResources.dark,
-                                  colorOpacity: 0.1,
-                                  borderRadius: BorderRadius.circular(19),
-                                  overlay: const SizedBox(
-                                      width: 357,
-                                      height: 137,
-                                      child: Center(
-                                        child: Text(
-                                          "Coming Soon...",
-                                          style: TextStyle(
-                                              color: ColorsResources.goldenColor,
-                                              fontSize: 57,
-                                              fontFamily: 'Handwriting'
-                                          ),
-                                        ),
-                                      )
-                                  ),
-                                  child: const SizedBox(
-                                      width: 357,
-                                      height: 137,
-                                      child: Center(
-                                        child: Text(
-                                          "Coming Soon...",
-                                          style: TextStyle(
-                                              color: ColorsResources.goldenColor,
-                                              fontSize: 59,
-                                              fontFamily: 'Handwriting'
-                                          ),
-                                        ),
-                                      )
-                                  )
-                              )
-                          )
-                      ),
-
-                      const Positioned(
-                          bottom: 37,
-                          left: 37,
-                          right: 37,
-                          child: SizedBox(
-                              height: 53,
-                              width: 53,
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: CircularProgressIndicator(
-                                  strokeCap: StrokeCap.round,
-                                  color: ColorsResources.goldenColor,
-                                ),
-                              )
-                          )
-                      )
-                      /* End - Coming Soon */
-
-                    ]
+                  ]
                 )
             )
         )
+    );
+  }
+
+  /*
+   * fwfwf
+   **/
+  Widget allContentsWidgets() {
+
+    return SlideTransition(
+        position: offsetAnimation,
+        child: ScaleTransition(
+            scale: doubleAnimation,
+            child: Stack(
+                children: [
+
+                  /* Start - Gradient Background - Dark */
+                  Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(17),
+                          topRight: Radius.circular(17),
+                          bottomLeft: Radius.circular(17),
+                          bottomRight: Radius.circular(17)
+                      ),
+                      border: Border(
+                          top: BorderSide(
+                            color: ColorsResources.black,
+                            width: 7,
+                          ),
+                          bottom: BorderSide(
+                            color: ColorsResources.black,
+                            width: 7,
+                          ),
+                          left: BorderSide(
+                            color: ColorsResources.black,
+                            width: 7,
+                          ),
+                          right: BorderSide(
+                            color: ColorsResources.black,
+                            width: 7,
+                          )
+                      ),
+                      gradient: LinearGradient(
+                          colors: [
+                            ColorsResources.premiumDark,
+                            ColorsResources.black,
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(1.0, 0.0),
+                          stops: [0.0, 1.0],
+                          transform: GradientRotation(-45),
+                          tileMode: TileMode.clamp
+                      ),
+                    ),
+                  ),
+                  /* End - Gradient Background - Dark */
+
+                  /* Start - Branding Transparent */
+                  Align(
+                    alignment: Alignment.center,
+                    child: Opacity(
+                        opacity: 0.1,
+                        child: Transform.scale(
+                            scale: 1.7,
+                            child: const Image(
+                              image: AssetImage("assets/logo.png"),
+                            )
+                        )
+                    ),
+                  ),
+                  /* End - Branding Transparent */
+
+                  /* Start - Gradient Background - Golden */
+                  Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(17),
+                                topRight: Radius.circular(17),
+                                bottomLeft: Radius.circular(17),
+                                bottomRight: Radius.circular(17)
+                            ),
+                            gradient: RadialGradient(
+                              radius: 1.1,
+                              colors: <Color> [
+                                ColorsResources.primaryColorLighter.withOpacity(0.51),
+                                Colors.transparent,
+                              ],
+                              center: const Alignment(0.79, -0.87),
+                            )
+                        ),
+                        child: SizedBox(
+                          height: calculatePercentage(99, displayHeight()),
+                          width: calculatePercentage(99, displayWidth()),
+                        ),
+                      )
+                  ),
+                  /* End - Gradient Background - Golden */
+
+                  /* Start - Purchase Plan Picker */
+                  const Positioned(
+                      right: 19,
+                      top: 19,
+                      child: PurchasePlanPicker()
+                  ),
+                  /* End - Purchase Plan Picker */
+
+                  /* Start - Menu */
+                  Positioned(
+                    left: 19,
+                    top: 19,
+                    child: SizedBox(
+                        height: 59,
+                        width: 59,
+                        child: InkWell(
+                            onTap: () {
+
+                              if (menuOpen) {
+
+                                menuOpen = false;
+
+                                animationController.reverse().whenComplete(() {
+
+                                });
+
+                              } else {
+
+
+                                menuOpen = true;
+
+                                animationController.forward().whenComplete(() {
+
+                                });
+                              }
+
+                            },
+                            child: const Image(
+                              image: AssetImage("assets/menu.png"),
+                            )
+                        )
+                    ),
+                  ),
+                  /* End - Menu */
+
+                ]
+            )
+        )
+    );
+  }
+
+  Widget menuItems() {
+
+    return Container(
+      color: Colors.black,
+      child: Stack(
+        children: [
+
+
+
+        ]
+      )
     );
   }
 
