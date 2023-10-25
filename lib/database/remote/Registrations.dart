3595839c848fa5a95f119fa07f2f2b1977a7b9fa
Elias Fazel <elias.fazel@gmail.com>
2023-10-25 08:14:26 -0700

    [Description]
    [ChangeLog]
    ~ Sachiels AI Website
    
    [Build][ChangeLog]
    # Libraries Updated

--- lib/database/remote/Registrations.dart
+++ lib/database/remote/Registrations.dart
@@ -1,13 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:sachiel_website/utils/io/file_io.dart';
 
 class Registrations {
 
   void preregistrationCandlestick(String emailAddress) async {
+    debugPrint(emailAddress);
 
-    // await FirebaseFirestore.instance.collection("/Sachiels/Candlesticks/Preregistrations")
-    //     .doc(emailAddress).set({
-    //       "Email": emailAddress
-    //     });
+    FirebaseFirestore.instance.collection("/Sachiels/Candlesticks/Preregistrations")
+        .doc(emailAddress).set({
+          "Email": emailAddress
+        });
 
     storeBoolean("Registered", true);
 
